44098912d99e5f0fc6f3acf0d06fc8a742e92c36
Melody Kimball <melody@forestkimball.com>
2025-01-09 10:07:24 -0500

    OAuth updates

--- src/OAuth/Factory/AuthResponseFactoryInterface.php
+++ src/OAuth/Factory/AuthResponseFactoryInterface.php
@@ -16,17 +16,41 @@ interface AuthResponseFactoryInterface
 
 
     /**
-     * @param string $code
      * @param string $state
+     * @param string $code
      * @param string|null $error
      * @param string|null $errorDescription
      * @param string|null $errorUri
      *
      * @return AuthResponseInterface
      */
-    public function createAuthResponse(
-        string $code,
+    public function createAuthCodeResponse(
         string $state,
+        string $code,
+        string|null $error = null,
+        string|null $errorDescription = null,
+        string|null $errorUri = null
+    ): AuthResponseInterface;
+
+
+    /**
+     * @param string $state
+     * @param string $idToken
+     * @param string|null $tokenType
+     * @param string|null $accessToken
+     * @param int|null $expiresIn
+     * @param string|null $error
+     * @param string|null $errorDescription
+     * @param string|null $errorUri
+     *
+     * @return AuthResponseInterface
+     */
+    public function createImplicitFlowResponse(
+        string $state,
+        string $idToken,
+        string|null $tokenType = null,
+        string|null $accessToken = null,
+        int|null $expiresIn = null,
         string|null $error = null,
         string|null $errorDescription = null,
         string|null $errorUri = null
